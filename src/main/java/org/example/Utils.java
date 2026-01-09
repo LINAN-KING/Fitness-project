9858f266b354259ef3d3333502c3362647237393
LINAN-KING <166568361@qq.com>
2026-01-09 21:41:12 +0800

    <feat>(Utils)；新增计数器功能

---
+++ src/main/java/org/example/Utils.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class Utils {
+    public Integer count(){
+        int count = 0;
+        for(int i = 0 ; i < 10 ; i++){
+            count += i;
+        }
+        return count;
+    }
+}
